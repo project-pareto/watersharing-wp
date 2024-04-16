75f9e37606afc734e92f808c95361debf5ed7ee2
Michael Fucilli <michael.fucilli@troyweb.com>
2024-04-16 13:30:58 -0400

    update git ignore and include missing build files for assets stylesheets/scripts and blocks

---
+++ blocks/build/consumption-dashboard/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => 'f68885b7f514184dbf02');
---
+++ blocks/build/consumption-form/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '9cae721cf48515566284');
