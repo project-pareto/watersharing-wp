d9bf088d75f1ee5801dcdcc3e9a947f95df2d8d8
dan-ledezma-twc <dan.ledezma@troyweb.com>
2024-08-07 10:11:07 -0400

    re-titling setting page

--- inc/watermanagement-settings.php
+++ inc/watermanagement-settings.php
@@ -11,7 +11,7 @@ function watermanagement_settings_page() {
 
 	echo "
 	<div class='wrap'>
-		<h1>Watermanagement Plugin Settings</h1>
+		<h1>Water Management Plugin Settings</h1>
 		<p>This plugin recreates the watersharing features of <a href='https://share.producedwater.org'>share.producedwater.org</a>, a water management prototype website, developed as part of a collaboration between teh Ground Water Protection Council (watersharing) and the US Department of Energy (DOE). The watersharing tool provides all of the features to collect information about produced water avaialability and needs from users, and suggests mutually beneficial trades that minimize transportation distances between matched users.</p>
 		<p>This plugin creates the necessary posts types, request forms, and dashboard to collect and store the users produced water information, it will then create an export JSON file of the data which can be used to compute a match.</p>
 		<p>Functionality to match users together is provided by a separate Python script that will have to be installed on your server separately. You can find this script with instructions in the <a href='https://github.com/project-pareto/watersharing'>Git Repo</a>.
