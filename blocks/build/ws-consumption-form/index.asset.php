68b20cee906ebe4ab5cf3ebe1307772ba5cd083c
dan-ledezma-twc <dan.ledezma@troyweb.com>
2024-09-03 13:21:56 -0400

    Initializing new blocks

---
+++ blocks/build/ws-consumption-form/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => 'b3e3fa1d943fbc6da685');
---
+++ blocks/build/wt-production-dashboard/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => '8b6fb52bf8a424c7389d');
